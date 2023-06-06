94454f480f75223b17d03bca4a207b4c1f713221
Danilo Silva <silvacdanilo1910@gmail.com>
2023-06-06 16:20:49 -0300

    mudança para tabela GalToLit

--- src/Exercicios/GalToLit.java
+++ src/Exercicios/GalToLit.java
@@ -3,13 +3,20 @@ package Exercicios;
 public class GalToLit {
     public static void main(String[] args) throws Exception {
         double gallons, liters;
+        int counter;
 
-        gallons = 10; // galões inicial
+        counter = 0; // inicio contador
+        gallons = 1; // inicio galões
+        for(gallons = 1; gallons <= 50; gallons++){
 
-        liters = gallons * 3.7854; // converte para litros
+            liters = gallons * 3.7854; // converte para litros
 
-
-
-        System.out.println(gallons + " gallons is "+ liters +" liters.");
+            System.out.println(gallons + " gallons is "+ liters +" liters.");
+            counter++;
+            if(counter == 10){
+                System.out.println();
+                counter = 0;
+            }
+        }
     }
 }

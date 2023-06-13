b81f8daba9eb7ed3baafd90f799c6e7e0a56baea
DanSilva1910 <132784618+DanSilva1910@users.noreply.github.com>
2023-06-13 20:52:14 -0300

    varias alterações

---
+++ src/Exercicios/Contador.java
@@ -0,0 +1,14 @@
+package Exercicios;
+
+public class Contador {
+    public static void main(String[] args) {
+        int inicio, fim;
+
+        for (inicio = 1000, fim = 0; inicio > 0; inicio = inicio - 2) {
+
+            System.out.println(inicio);
+
+        }
+
+    }
+}
